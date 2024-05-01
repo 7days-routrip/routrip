26dab6b3741bfbeb2742f1dce28580b571fa021d
kimhongrae <khr0690@hanmail.net>
2024-05-02 03:07:50 +0900

    feat : 푸터 컴포넌트 구현

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,9 +1,28 @@
 import styled from "styled-components";
+import FooterLogo from "../../../public/assets/images/logo-footer.png";
 
 const Footer = () => {
-  return <FooterStyle>Footer</FooterStyle>;
+  return (
+    <FooterStyle>
+      <img src={FooterLogo} alt="footer logo" />
+      <div className="container">
+        <div>2024, 7days</div>
+        <div>copyrightⓒ</div>
+      </div>
+    </FooterStyle>
+  );
 };
 
-const FooterStyle = styled.div``;
+const FooterStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  border-top: 1px solid #e7e7e7;
+  gap: 20px;
+  height: 150px;
+  width: 100%;
+  max-width: 1080px;
+`;
 
 export default Footer;
