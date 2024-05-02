010988da36594b74ff37236af54ff543f0d2c3d8
codedung <dungdung9376@gmail.com>
2024-05-02 17:17:08 +0900

    feat: [ISSUE#19] 인가 시스템 작업 + typeORM 설정

--- server/src/middlewares/authorization.ts
+++ server/src/middlewares/authorization.ts
@@ -1,8 +1,35 @@
-import { Request, Response, NextFunction } from "express";
+import { pool } from "@/config/mariadb";
+import { NextFunction, Request, Response } from "express";
+
+const getSql = (type: string): string => {
+  const sql = `select userId from ${type} where id = ?`;
+  return sql;
+};
 
 const authorization = async (req: Request, res: Response, next: NextFunction) => {
-  const itemId: number = parseInt(req.params.id);
-  const userId: number = req.body.user.id;
+  const conn = await pool.getConnection();
+  try {
+    if (typeof req.user !== "undefined") {
+      const itemId: number = parseInt(req.params.id);
+      const userId: number = req.user.id;
+      let sql: string;
+
+      if (req.path.includes("trips")) {
+        sql = getSql("jeurneys");
+      } else if (req.path.includes("comments")) {
+        sql = getSql("comments");
+      } else if (req.path.includes("posts")) {
+        sql = getSql("posts");
+      }
+
+      // const [match] = await conn.query(sql, values);
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      if (err.message === "") {
+      }
+    }
+  }
 };
 
 export { authorization };
