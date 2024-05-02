d2e417111b756f452ef82fc6d54bfac72386fe64
kimhongrae <khr0690@hanmail.net>
2024-05-02 17:26:54 +0900

    style: 글쓰기 및 top버튼 스타일 수정

--- client/src/components/common/WriteTopBtn.tsx
+++ client/src/components/common/WriteTopBtn.tsx
@@ -1,47 +1,49 @@
+import React from "react";
 import styled from "styled-components";
-import { theme } from "../../styles/theme";
 import icons from "@/icons/icons";
+import Button from "./Button";
 
 const WriteTopBtnStyle = styled.div`
   display: flex;
   gap: 20px;
   justify-content: center;
-  margin-top: 20px;
+  align-items: center;
+  margin-top: -60px;
+  z-index: 1;
+  min-height: 50px;
 `;
 
-const Button = styled.button<{ primary?: boolean }>`
-  border-radius: ${theme.borderRadius.write};
-  border: 1px solid ${theme.color.primary};
-  background-color: ${(props) => (props.primary ? theme.color.primary : theme.color.white)};
-  color: ${(props) => (props.primary ? theme.color.white : theme.color.primary)};
-  font-size: ${theme.buttonSize.medium.fontSize};
-  padding: ${theme.buttonSize.small.padding};
-  cursor: pointer;
-
-  &:hover {
-    opacity: 0.8;
-  }
+const WriteButton = styled(Button)`
+  padding: 0.5rem 0.6rem;
 `;
 
-// 컴포넌트 기능 구현
-const WriteTopBtn = () => {
+const TopButton = styled(Button)`
+  padding: 0.5rem 0.6rem;
+`;
+
+interface WriteTopBtnProps {
+  isWriting: boolean;
+}
+
+const WriteTopBtn: React.FC<WriteTopBtnProps> = ({ isWriting }) => {
   const handleScrollTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
-
   const { TopIcon } = icons;
 
   return (
     <WriteTopBtnStyle>
-      <Button primary={false} onClick={() => (window.location.href = "/write")}>
-        글쓰기
-      </Button>
-      <Button primary onClick={handleScrollTop}>
+      {isWriting && (
+        <WriteButton size="medium" scheme="secondary" radius="write" onClick={() => (window.location.href = "/write")}>
+          글쓰기
+        </WriteButton>
+      )}
+      <TopButton size="small" scheme="primary" radius="write" onClick={handleScrollTop} style={{ borderRadius: "50%" }}>
         <TopIcon color="white" />
-      </Button>
+      </TopButton>
     </WriteTopBtnStyle>
   );
 };
