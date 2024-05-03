ea27430afa3107d5013f32078d91763c0b454bc5
lugia574 <66871221+lugia574@users.noreply.github.com>
2024-05-03 16:29:55 +0900

    feat : useAuth 대략적인 구현

---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,41 @@
+import { authJoin, authLogin } from "@/apis/auth.api";
+import { useAuthStore } from "@/stores/authStore";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// 로그인
+export interface LoginProps {
+  email: string;
+  password: string;
+}
+
+// 로그인
+export interface JoinProps extends LoginProps {
+  nickname: string;
+}
+
+export const useAuth = () => {
+  const navigate = useNavigate();
+  // 상태
+  const { storeLogin } = useAuthStore();
+  const [resetRequested, setResetRequested] = useState(false);
+
+  const userLogin = async (data: LoginProps) => {
+    try {
+      const loginRes = await authLogin(data);
+      storeLogin(loginRes.Authorization, loginRes.nickname);
+      navigate("/");
+    } catch (error) {
+      // 로그인 실패
+    }
+  };
+
+  const userJoin = async (data: JoinProps) => {
+    try {
+      const joinRes = await authJoin(data);
+      navigate("/login");
+    } catch (error) {
+      // 회원가입 실패
+    }
+  };
+};
