349f74bb83572d739eea6a5e4c521c5a9776c058
jh9788 <gkwhdgns2@gmail.com>
2024-05-05 18:49:29 +0900

    feat: 장소 중복 체크, 장소 상세 요청 구현

--- server/src/routes/spots.routes.ts
+++ server/src/routes/spots.routes.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response, NextFunction } from "express";
-import { Repository } from "typeorm";
-import { StatusCodes } from "http-status-codes";
-import { AppDataSource } from "@/config/ormSetting";
-import { Places } from "@/models/places.model";
-import { regNewPlace } from "@/controller/spots.ctl";
+import { addToPlace, checkDuplicatePlaces, getPlaceDetail } from "@/controller/spots.ctl";
 
 const router = express.Router();
 router.use(express.json());
 
 // 신규 장소 등록
-router.post("/", regNewPlace);
+router.post("/", addToPlace);
+router.post("/check/:id", checkDuplicatePlaces);
+router.get("/");
+router.get("/:id", getPlaceDetail);
 
 export default router;
