f2ad2daa0bc3b5b76d989a9973efc8109a96bfa1
jh9788 <gkwhdgns2@gmail.com>
2024-05-06 23:28:39 +0900

    feat: 장소 검색 구현

--- server/src/routes/spots.routes.ts
+++ server/src/routes/spots.routes.ts
@@ -1,6 +1,5 @@
-
 import express, { Request, Response, NextFunction } from "express";
-import { addToPlace, checkDuplicatePlaces, getPlaceDetail } from "@/controller/spots.ctl";
+import { addToPlace, checkDuplicatePlaces, getPlaceDetail, searchPlace } from "@/controller/spots.ctl";
 
 const router = express.Router();
 router.use(express.json());
@@ -8,7 +7,7 @@ router.use(express.json());
 // 신규 장소 등록
 router.post("/", addToPlace);
 router.post("/check/:id", checkDuplicatePlaces);
-router.get("/");
+router.get("/", searchPlace);
 router.get("/:id", getPlaceDetail);
 
 export default router;
