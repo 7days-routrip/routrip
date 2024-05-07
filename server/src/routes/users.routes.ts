1866f308f56ad1e557e10278b77461b0bcd89da8
6uamy <hst007@naver.com>
2024-05-07 15:36:49 +0900

    feat: 이메일, 닉네임 중복 확인 API 구현

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -4,6 +4,9 @@ import usersController from "@/controller/users.ctl";
 const router = express.Router();
 
 router.post("/join", usersController.join);
-router.get("/login", usersController.login);
+router.post("/login", usersController.login);
+
+router.post("/check/email", usersController.checkEmail);
+router.post("/check/nickname", usersController.checkNickname);
 
 export default router;
