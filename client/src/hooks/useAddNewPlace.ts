846a8e322307a2a8607ff20ce3c5bc5996838f5a
khe <khj778878@naver.com>
2024-05-10 15:31:14 +0900

    chore: useAddNewPlace 커스텀 훅 주석 제거 및 주석 수정

--- client/src/hooks/useAddNewPlace.ts
+++ client/src/hooks/useAddNewPlace.ts
@@ -36,8 +36,8 @@ export const useAddNewPlace = (data: Place) => {
           throw { type: errorType.getPlaceDetailApiError, error: getPlaceDetailApiError };
         }
 
-        // 404 에러인 경우 -> 아직 DB에 저장되지 않은 장소이므로, 추가 요청 로직 수행
-        // a. 구글 api로 장소 상세 정보 요청(장소 이미지 포함)
+        // 404 에러 -> 추가 요청 진행
+        // a. 구글 api로 장소 상세 정보 요청
         const [placeDataFromGoogleError, placeDataFromGoogle] = await to(getPlaceDetail(googleMap, data.id));
 
         if (!placeDataFromGoogle) throw { type: errorType.placeDataFromGoogleError, error: placeDataFromGoogleError };
@@ -65,15 +65,14 @@ export const useAddNewPlace = (data: Place) => {
       };
 
       if (isNew) {
-        // 3. 백엔드 장소 세부 정보 요청에 대해 404 에러를 받고, 추가 로직을 진행한 경우
+        // 3. 신규 장소 등록 요청 성공한 경우
         addPlace(addData);
         setMarkerType("add");
 
         return;
       }
 
-      // 4. 백엔드 장소 세부 정보 요청에 대해 200 OK를 받은 경우
-      // 사용자의 선택을 확인하고 해당 장소를 프론트 전역 상태에 추가
+      // 4. 백엔드 장소 세부 정보 요청 성공한 경우
       showConfirm("이미 등록된 장소입니다.\n해당 장소를 추가하시겠어요?", () => {
         addPlace(addData);
         setMarkerType("add");
@@ -82,13 +81,10 @@ export const useAddNewPlace = (data: Place) => {
 
     onError: async (err: any) => {
       if (err.type === errorType.getPlaceDetailApiError) {
-        // 백엔드 서버로 세부 장소 조회 요청 error (404 제외)
         showAlert("장소 정보를 불어오는데 실패했습니다.\n잠시 후에 다시 시도해주세요.", "error");
       } else if (err.type === errorType.placeDataFromGoogleError) {
-        // 구글 api로 장소 상세 정보를 요청 error
         showAlert("해당 장소에 대한 정보를 불러올 수 없습니다.\n다른 장소를 등록해주세요.", "error");
       } else if (err.type === errorType.addNewPlaceApiError) {
-        // 백엔드 서버로 신규 장소 등록 요청 error
         showAlert("해당 장소를 등록하는데 문제가 발생했습니다.\n잠시 후에 다시 시도해주세요.", "error");
       }
     },
