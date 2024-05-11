1975907204028a3a20b3b9536ed8049d158f2326
khe <khj778878@naver.com>
2024-05-11 20:25:04 +0900

    chore: mock 데이터 반환 코드 주석처리, 실제 api 호출 주석 제거

--- client/src/apis/schedule.api.ts
+++ client/src/apis/schedule.api.ts
@@ -37,26 +37,27 @@ export const addNewSchedule = async (
       days,
     };
 
-    console.log(bodyData);
-    return bodyData;
-    // const { data } = await httpClient.post("/journeys", bodyData);
-    // return data;
+    // console.log(bodyData);
+    // return bodyData;
+    const { data } = await httpClient.post("/journeys", bodyData);
+    return data;
   } catch (err: any) {
-    showAlert("일정을 등록에 실패했습니다.\n문제가 지속될 경우 고객센터로 문의해주세요.", "error");
+    showAlert("일정 등록에 실패했습니다.\n문제가 지속될 경우 고객센터로 문의해주세요.", "error");
     return;
   }
 };
 
 export const getScheduleDetails = async (id: string) => {
   try {
-    // const { data } = await httpClient.get<ScheduleDetails>(`/journeys/${id}`);
-    // const convertData = convertScheduleDetails(data);
-    // return convertData;
-    const mockScheduleData = convertScheduleDetails(mockScheduleDetailData);
-    console.log(mockScheduleData);
+    const { data } = await httpClient.get<ScheduleDetails>(`/journeys/${id}`);
+    const convertData = convertScheduleDetails(data);
+    return convertData;
+    // const mockScheduleData = convertScheduleDetails(mockScheduleDetailData);
+    // console.log(mockScheduleData);
 
-    return mockScheduleData;
+    // return mockScheduleData;
   } catch (err) {
+    console.error("api ", err);
     throw err;
   }
 };
