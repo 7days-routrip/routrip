99293b50955391913931b51e2c7ac4e06142a245
khe <khj778878@naver.com>
2024-05-11 19:34:41 +0900

    refactor: 지도 경계 계산하는 코드를 함수로 만들어서 분리

--- client/src/components/map/ScheduleGoogleMap.tsx
+++ client/src/components/map/ScheduleGoogleMap.tsx
@@ -63,6 +63,23 @@ const ScheduleGoogleMap = () => {
   const { bookmarkPlaces } = useBookmarkPlacesStore();
   const [clickMarker, setClickMarker] = useState<SelectedPlace | Place | null>(null);
 
+  const getPlaceArr = () => {
+    switch (markerType) {
+      case "add":
+        return addPlaces;
+      case "day":
+        return dayPlaces[dayIndex as number];
+      case "searchApi":
+        return searchPlaces;
+      case "searchGoogle":
+        return nearPlaces;
+      case "bookmarkList":
+        return bookmarkPlaces;
+      default:
+        return [];
+    }
+  };
+
   const handleChanged = useCallback(() => {
     if (googleMap && googleMap.getCenter()) {
       setCenter({
@@ -86,9 +103,9 @@ const ScheduleGoogleMap = () => {
 
     if (googleMap) {
       googleMap.panTo(place.location); // 1. 마커 위치로 지도 이동
+
       const currentZoom = googleMap.getZoom() || 6;
-      // console.log(currentZoom);
-      const targetZoom = Math.max(currentZoom, 15);
+      const targetZoom = Math.max(currentZoom, 14);
       googleMap.setZoom(targetZoom); // 2. 줌 비율 조정(확대)
     }
   };
@@ -96,6 +113,7 @@ const ScheduleGoogleMap = () => {
   const onClickMap = () => {
     if (clickMarker) {
       setClickMarker(null);
+      // updateMapBounds(googleMap, getPlaceArr());
     }
   };
 
@@ -119,26 +137,7 @@ const ScheduleGoogleMap = () => {
     // 지도에 표시할 마커가 전부 보이도록 지도 경계선을 계산
     if (!googleMap) return;
 
-    let placeArr;
-    switch (markerType) {
-      case "add":
-        placeArr = addPlaces;
-        break;
-      case "day":
-        placeArr = dayPlaces[dayIndex as number];
-        break;
-      case "searchApi":
-        placeArr = searchPlaces;
-        break;
-      case "searchGoogle":
-        placeArr = nearPlaces;
-        break;
-      case "bookmarkList":
-        placeArr = bookmarkPlaces;
-        break;
-    }
-
-    updateMapBounds(googleMap, placeArr);
+    updateMapBounds(googleMap, getPlaceArr());
   }, [addPlaces, dayPlaces, markerType, dayIndex, googleMap, searchPlaces, nearPlaces]);
 
   return isLoaded ? (
