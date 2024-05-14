3708e4e63f57d4fbf846563895b809e15b5655a1
khe <khj778878@naver.com>
2024-05-15 05:06:50 +0900

    refactor: 카테고리 이미지 경로 추가

--- client/src/constants/categories.ts
+++ client/src/constants/categories.ts
@@ -1,10 +1,19 @@
+import china from "/assets/images/china.png";
+import france from "/assets/images/france.png";
+import japan from "/assets/images/japan.png";
+import usa from "/assets/images/usa.png";
+import switzerland from "/assets/images/switzerland.png";
+import australia from "/assets/images/australia.png";
+import philippines from "/assets/images/philippines.png";
+import vietnam from "/assets/images/vietnam.png";
+
 export const categories = [
-  { id: 2, name: "일본" },
-  { id: 3, name: "중국" },
-  { id: 6, name: "베트남" },
-  { id: 14, name: "스위스" },
-  { id: 27, name: "미국" },
-  { id: 30, name: "호주" },
-  { id: 5, name: "필리핀" },
-  { id: 11, name: "프랑스" },
+  { id: 2, name: "일본", img: japan },
+  { id: 3, name: "중국", img: china },
+  { id: 6, name: "베트남", img: vietnam },
+  { id: 14, name: "스위스", img: switzerland },
+  { id: 27, name: "미국", img: usa },
+  { id: 30, name: "호주", img: australia },
+  { id: 5, name: "필리핀", img: philippines },
+  { id: 11, name: "프랑스", img: france },
 ];
