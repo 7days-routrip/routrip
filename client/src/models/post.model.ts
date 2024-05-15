839f1f14a03686148cd5d58fc71137bef9097c8f
kimhongrae <khr0690@hanmail.net>
2024-05-16 04:30:54 +0900

    fix : post 모델 수정

--- client/src/models/post.model.ts
+++ client/src/models/post.model.ts
@@ -1,16 +1,22 @@
-// 임의로 해논거 이거 나중에 변경 해야해함
 export interface Post {
   id: number;
   title: string;
   date: string;
   author: string;
   profileImg: string;
-  continental: string;
-  country: string;
+  continent: {
+    id: number;
+    name: string;
+  };
+  country: {
+    id: number;
+    name: string;
+  };
   commentsNum: string;
   likesNum: string;
   postsImg: string;
   createdAt: string;
+  conetents: string;
 }
 
 export interface Pagination {
