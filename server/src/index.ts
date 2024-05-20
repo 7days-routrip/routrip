8cbac43176577c07cdc9347c7d89e075fbaec9b3
jh9788 <gkwhdgns2@gmail.com>
2024-05-20 18:19:42 +0900

    deploy: server 포트 http도 열기

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,3 +4,8 @@ const PORT = 1234;
 server.listen(PORT, () => {
   console.log("HTTPS server listening on port " + PORT);
 });
+
+const PORT_HTTP = 80;
+app.listen(PORT_HTTP, () => {
+  console.log("HTTP server listening on port" + PORT_HTTP);
+});
