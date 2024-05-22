844d1c0f40c34d19d5b1c0a90eb0d8105d49a0c7
lugia574 <66871221+lugia574@users.noreply.github.com>
2024-05-23 02:07:51 +0900

    style : 프로필 에디터 페이지 수정

--- client/src/components/common/Comment.tsx
+++ client/src/components/common/Comment.tsx
@@ -37,12 +37,12 @@ const CommentCard = ({ commentProps }: Props) => {
 
 const CommentStyle = styled.div`
   width: 100%;
-  min-width: 370px;
   padding: 0 1.5rem 0.6rem;
   border-bottom: 1px solid ${({ theme }) => theme.color.borderGray};
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
+  gap: 0.5rem;
 
   &:last-child {
     border-bottom: unset;
--- client/src/pages/ProfileEditPage.tsx
+++ client/src/pages/ProfileEditPage.tsx
@@ -104,9 +104,9 @@ const ProfileEditPage = () => {
     if (formImage) {
       userProfileImage(formImage);
     }
-    clearErrors;
     showAlert("수정이 완료되었습니다.", "logo", () => {
       profileRefetch();
+      clearErrors("nickname");
     });
     // setPreviewUpdate(true);
   };
